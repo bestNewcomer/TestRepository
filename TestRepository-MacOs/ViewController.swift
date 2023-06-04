a165a597d04d0f01d09e1e7f6efc1ef127e6bd36
bestNewcomer <NewcomerRUS@yandex.ru>
2023-06-04 12:18:31 +0300

    Инициируем проект

---
+++ TestRepository-MacOs/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestRepository-MacOs
+//
+//  Created by Ринат Шарафутдинов on 04.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
